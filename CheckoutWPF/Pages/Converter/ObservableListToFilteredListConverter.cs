29ab213d64866eee2b3f49d05ee270d35ad8ced6
Benedikt Jakobs <benedikt.jakobs@outlook.de>
2015-05-30 16:18:08 +0200

    changed filtering method of product list, added first content to myproductspage, refactoring

--- CheckoutWPF/Pages/Converter/ObservableListToFilteredListConverter.cs
+++ CheckoutWPF/Pages/Converter/ObservableListToFilteredListConverter.cs
@@ -12,10 +12,8 @@ using System.Windows.Data;
 
 namespace CheckoutWPF.Pages.Converter
 {
-    public class ObservableListToCollectionViewConverter : IMultiValueConverter
+    public class ObservableListToFilteredListConverter : IMultiValueConverter
     {
-        private int currentCategoryId;
-
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             if (values.Length != 2)
@@ -24,21 +22,14 @@ namespace CheckoutWPF.Pages.Converter
             if (values[0] is ObservableCollection<Product> && values[1] is int)
             {
                 var list = (ObservableCollection<Product>)values[0];
-                currentCategoryId = (int)values[1];
+                var currentCategoryId = (int)values[1];
 
-                var view = CollectionViewSource.GetDefaultView(list);
-                view.Filter = new Predicate<object>(FilterByCurrentCategoryId);
-                return view;
+                var filteredList = list.Where(x => x.CategoryId == currentCategoryId);
+
+                return filteredList;
             }
 
             return DependencyProperty.UnsetValue;
-        }
-
-        private bool FilterByCurrentCategoryId(object obj)
-        {
-            var item = (Product)obj;
-
-            return item.CategoryId == currentCategoryId;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
