7390c71c66d84fc7c1b6fa276f8ec19259ad8587
Benedikt Jakobs <benedikt.jakobs@outlook.de>
2015-07-08 13:11:09 +0200

    refactoring

---
+++ CheckoutWPF/Pages/Converter/CategoryIdToCountConverter.cs
@@ -0,0 +1,28 @@
+﻿using CheckoutWPF.DataSource;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CheckoutWPF.Pages.Converter
+{
+    public class CategoryIdToCountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var categoryId = (int)value;
+
+            var count = ProductRepository.Instance.Products.Count(x => x.CategoryId == categoryId);
+
+            return count;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
